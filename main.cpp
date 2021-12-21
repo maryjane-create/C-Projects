83482258eb101f357fa26630b507f76d3c294e01
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2021-12-21 11:40:58 +0100

    added first cpp project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::cout << "H\r\"el\alo,\t World!\"" << std::endl;
+    std::cout<<"\\welcome\n to c++\\\n";
+std::cout<<"welcome to c++ ";
+std::cout<<"programming\n";
+
+int num1=0;
+int num2{0};
+std::cout<<"please enter number ";
+std::cin>>num1;
+std::cout<<"please enter a second number ";
+std::cin>>num2;
+int sum=num1+num2;
+std::cout<<"sum is "<<sum <<std::endl;
+int num4{0}, num5{9},num3{3},num6{9}  ;
+
+num4=(num1+num2+num3)/2*num1;
+std::cout<<"number four is "<<num4<<std::endl;
+//multiplication comes first in every programming language followed by modulo operator
+//assignment comes last
+    return 0;
+}
